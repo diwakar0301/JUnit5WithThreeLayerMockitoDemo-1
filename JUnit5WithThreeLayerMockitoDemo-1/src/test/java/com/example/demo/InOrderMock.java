74bb937123755eca9de2c725da219a8f89cd3499
diwakar0301 <86709309+diwakar0301@users.noreply.github.com>
2022-05-10 17:09:49 +0530

    TestNGWithMockitoWithBeforeMethod

--- JUnit5WithThreeLayerMockitoDemo-1/src/test/java/com/example/demo/InOrderMock.java
+++ JUnit5WithThreeLayerMockitoDemo-1/src/test/java/com/example/demo/InOrderMock.java
@@ -1,46 +1,42 @@
 package com.example.demo;
-
-
+//import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
 import com.example.controller.HomeController;
 import com.example.service.ServiceInterface1;
-
-
+import org.junit.runner.RunWith;
+import org.testng.annotations.BeforeMethod;
+import org.mockito.MockitoAnnotations;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class InOrderMock {
-	
 	@InjectMocks
-	private HomeController homeController=new HomeController();
-	
+	private HomeController homeController=new HomeController();	
 	@Mock
-	private ServiceInterface1 serviceInterface1;
-	
-	
+	private ServiceInterface1 serviceInterface1
+	//=mock(ServiceInterface1.class)
+	;
+@BeforeMethod
+	public void setup() {
+		System.out.println("@BeforeMethod TestNGMockAnnotationExample");
+		MockitoAnnotations.initMocks(this);
+	}
 	@Test
 	public void retTest() throws Exception{
 		//assertEquals(homeController.ret(),"hello from ret method");
-		
-		
-		
-		
 		when(serviceInterface1.add(10, 20)).thenReturn(30);
 		when(serviceInterface1.ret()).thenReturn("a"); 
 		assertEquals(homeController.add(10, 20),30);
-		assertEquals(homeController.ret(),"a");
-		
+		assertEquals(homeController.ret(),"a");		
 		InOrder inOrder=inOrder(serviceInterface1);
-		
 		inOrder.verify(serviceInterface1).add(10,20);
 		inOrder.verify(serviceInterface1).ret();
-		
 	}
-
 }
